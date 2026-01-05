c7a8914c019085336d8d601af9dbaf7251b1c131
tymanmimo <tymanmimo@gmail.com>
2026-01-06 01:30:58 +0300

    Project init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Hello dude ${client.user?.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
